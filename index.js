1f79527a4fa28b69f92305c7e2038bc50709137c
NMD306 <167544328+NMD306@users.noreply.github.com>
2025-03-21 20:42:44 +0700

    Tich hop static files

--- index.js
+++ index.js
@@ -5,22 +5,24 @@ const app = express();
 const port = 3000;
 
 // thiet lap cho phan views
-app.set('views', path.join(__dirname, "views"))
-app.set('view engine', 'pug')
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+// thiet lap cho static files for FE
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.render("client/pages/home",{
-    pageTitle: "Trang Chu"
-  })
+  res.render("client/pages/home", {
+    pageTitle: "Trang Chu",
+  });
 });
 
 app.get("/tours", (req, res) => {
-    res.send("client/pages/tour-list", {
-        pageTitle: "Danh sach tour"
-    });
+  res.send("client/pages/tour-list", {
+    pageTitle: "Danh sach tour",
   });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-

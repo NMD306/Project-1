3dc4cf03ef8a4a7946cbd8b61c434f4d37f5dd30
NMD306 <167544328+NMD306@users.noreply.github.com>
2025-03-21 20:23:39 +0700

    Tich Hop Pug

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 const port = 3000;
 
+// thiet lap cho phan views
+app.set('views', path.join(__dirname, "views"))
+app.set('view engine', 'pug')
+
 app.get("/", (req, res) => {
-  res.send("Trang Chu");
+  res.render("client/pages/home",{
+    pageTitle: "Trang Chu"
+  })
 });
 
 app.get("/tours", (req, res) => {
-    res.send("Danh sach tour");
+    res.send("client/pages/tour-list", {
+        pageTitle: "Danh sach tour"
+    });
   });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+

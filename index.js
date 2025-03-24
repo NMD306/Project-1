d5f5c89287e22e8430f810db4d50f279ac4713d7
NMD306 <167544328+NMD306@users.noreply.github.com>
2025-03-24 20:50:32 +0700

    tach controller

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ require("dotenv").config();
 mongoose.connect(process.env.DATABASE);
 const path = require("path");
 
-const Tour = require("./models/tour.model")
+const tourController = require("./controllers/client/tour.controller");
+const homeController = require("./controllers/client/home.controller");
 
 const app = express();
 const port = 3000;
@@ -16,20 +17,9 @@ app.set("view engine", "pug");
 // thiet lap cho static files for FE
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
-  res.render("client/pages/home", {
-    pageTitle: "Trang Chu",
-  });
-});
+app.get("/", homeController.home);
 
-app.get("/tours", async (req, res) => {
-  const tourList = await Tour.find();
-
-  res.render("client/pages/tour-list", {
-    pageTitle: "Danh sach tour",
-    tourList: tourList,
-  });
-});
+app.get("/tours", tourController.list);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
